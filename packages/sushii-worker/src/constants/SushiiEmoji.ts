7d4c53357ebaefe2c473facca4c3ea4b0f0cc0bc
drklee3 <derrick@dlee.dev>
2023-02-06 21:23:35 -0800

    Fix reason error response

--- packages/sushii-worker/src/constants/SushiiEmoji.ts
+++ packages/sushii-worker/src/constants/SushiiEmoji.ts
@@ -2,6 +2,7 @@ enum SushiiEmoji {
   MessageEdit = "<:messageEdit:1046473005913608204>",
   MessageDelete = "<:messageDelete:1046224934722097192>",
   SushiiDot = "<:sushiiDot:1072383585442594867>",
+  BlueDot = "<:blueDot:1072383477577691199>",
 }
 
 export default SushiiEmoji;
