0cdad91d32fd14abcc90bfc4067d0ad49e5b99f2
drklee3 <derrick@dlee.dev>
2023-07-02 17:14:03 -0700

    Add rank test

---
+++ packages/sushii-worker/src/interactions/user/rank.test.ts
@@ -0,0 +1,66 @@
+import UserLevelProgress from "./rank.entity";
+
+describe("UserLevelProgress", () => {
+  describe.each([
+    {
+      xp: 0,
+      wantLevel: 1,
+      wantNextLevelXpProgress: 0,
+      wantNextLevelXpRequired: 100,
+    },
+    {
+      xp: 100,
+      wantLevel: 2,
+      wantNextLevelXpProgress: 0,
+      wantNextLevelXpRequired: 200,
+    },
+    {
+      xp: 300,
+      wantLevel: 3,
+      wantNextLevelXpProgress: 0,
+      wantNextLevelXpRequired: 300,
+    },
+    {
+      xp: 300 + 150,
+      wantLevel: 3,
+      wantNextLevelXpProgress: 150, // This is current progress
+      wantNextLevelXpRequired: 300, // Stays the same for total
+    },
+    {
+      xp: 600,
+      wantLevel: 4,
+      wantNextLevelXpProgress: 0,
+      wantNextLevelXpRequired: 400,
+    },
+    {
+      xp: 4500,
+      wantLevel: 10,
+      wantNextLevelXpProgress: 0,
+      wantNextLevelXpRequired: 1000,
+    },
+  ])(
+    "UserLevelProgress($xp XP)",
+    ({ xp, wantLevel, wantNextLevelXpProgress, wantNextLevelXpRequired }) => {
+      const levelProg = new UserLevelProgress(xp);
+
+      test("getLevel", () => {
+        const level = UserLevelProgress.getLevel(xp);
+
+        expect(level).toEqual(wantLevel);
+        expect(levelProg.level).toEqual(level);
+      });
+
+      test("nextLevel", () => {
+        expect(levelProg.nextLevelXpPercentage).toBeLessThanOrEqual(100);
+        expect(levelProg.nextLevelXpPercentage).toBeGreaterThanOrEqual(0);
+
+        expect(levelProg.nextLevelXpRequired).toEqual(wantNextLevelXpRequired);
+        expect(levelProg.nextLevelXpProgress).toEqual(wantNextLevelXpProgress);
+        expect(levelProg.nextLevelXpPercentage).toEqual(
+          (levelProg.nextLevelXpProgress / levelProg.nextLevelXpRequired) *
+            100.0
+        );
+      });
+    }
+  );
+});
