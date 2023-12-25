91c546b2d55dce906d83c9481ba097503f819d9b
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2023-12-24 20:07:26 -0800

    refactor(worker): Migrate rolemenu autocomplete to kysely

--- packages/sushii-worker/src/interactions/roles/RoleMenuAutocomplete.ts
+++ packages/sushii-worker/src/interactions/roles/RoleMenuAutocomplete.ts
@@ -2,6 +2,8 @@ import { ApplicationCommandOptionType } from "discord-api-types/v10";
 import { AutocompleteFocusedOption, AutocompleteInteraction } from "discord.js";
 import Context from "../../model/context";
 import { AutocompleteHandler } from "../handlers";
+import { searchRoleMenus } from "../../db/RoleMenu/ModLog.repository";
+import db from "../../model/db";
 
 export default class RoleMenuAutocomplete extends AutocompleteHandler {
   fullCommandNamePath = [
@@ -29,14 +31,15 @@ export default class RoleMenuAutocomplete extends AutocompleteHandler {
       throw new Error("Option type must be string.");
     }
 
-    const matching = await ctx.sushiiAPI.sdk.searchRoleMenuStartingWith({
-      guildId: interaction.guildId,
-      menuNameStartsWith: option.value,
-    });
+    const matching = await searchRoleMenus(
+      db,
+      interaction.guildId,
+      option.value,
+    );
 
-    const choices = matching.allRoleMenus?.nodes.slice(0, 25).map((s) => ({
-      name: s.menuName,
-      value: s.menuName,
+    const choices = matching.slice(0, 25).map((s) => ({
+      name: s.menu_name,
+      value: s.menu_name,
     }));
 
     await interaction.respond(choices || []);
