4a90c94dbd06d602e9861ef4a7e5e22703cb2849
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2024-12-04 21:27:11 -0800

    feat: Confirm button when deleting reason dm

--- packages/sushii-worker/src/interactions/moderation/reason/DMButton.ts
+++ packages/sushii-worker/src/interactions/moderation/reason/DMButton.ts
@@ -1,4 +1,11 @@
-import { ButtonInteraction, EmbedBuilder } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonInteraction,
+  ButtonStyle,
+  ComponentType,
+  EmbedBuilder,
+} from "discord.js";
 import Context from "../../../model/context";
 import customIds from "../../customIds";
 import { ButtonHandler } from "../../handlers";
@@ -8,6 +15,7 @@ import db from "../../../model/db";
 import { newModuleLogger } from "../../../logger";
 import { buildModLogComponents } from "../../../events/ModLogHandler";
 import { ActionType } from "../ActionType";
+import { sleep } from "bun";
 
 const logger = newModuleLogger("ModLog-DeleteDM-Button");
 
@@ -32,6 +40,72 @@ export default class DeleteModLogDMButtonHandler extends ButtonHandler {
       );
     }
 
+    // Ask for confirmation
+    const confirmationEmbed = new EmbedBuilder()
+      .setTitle("Are you sure?")
+      .setDescription(
+        "This will delete the DM sent to the user containing the reason.",
+      )
+      .setColor(Color.Warning);
+
+    const confirmationCancelButton = new ButtonBuilder()
+      .setCustomId("cancel")
+      .setLabel("Cancel")
+
+      .setStyle(ButtonStyle.Secondary);
+    const confirmationYesButton = new ButtonBuilder()
+      .setCustomId("yes")
+      .setLabel("Yes")
+      .setStyle(ButtonStyle.Danger);
+
+    const confirmationRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      confirmationCancelButton,
+      confirmationYesButton,
+    );
+
+    const confirmReplyMsg = await interaction.reply({
+      embeds: [confirmationEmbed],
+      components: [confirmationRow],
+      ephemeral: true,
+      // Required to await message component due to bug:
+      // https://github.com/discordjs/discord.js/issues/7992
+      // fetchReply: true,
+    });
+
+    const confirmMsg = await confirmReplyMsg.fetch();
+
+    let confirmButtonInteraction;
+    try {
+      confirmButtonInteraction = await confirmMsg.awaitMessageComponent({
+        // Only accept from user who initiated the interaction
+        // filter: (i) => i.user.id === interaction.user.id,
+        time: 1000 * 60 * 2, // 2 minutes
+        componentType: ComponentType.Button,
+      });
+
+      if (confirmButtonInteraction.customId === "cancel") {
+        await confirmButtonInteraction.update({
+          content: "Cancelled!",
+          components: [],
+          embeds: [],
+        });
+
+        // Wait for 5 seconds before deleting the message
+        await sleep(5 * 1000);
+
+        await confirmButtonInteraction.deleteReply();
+
+        return;
+      } else {
+        await confirmButtonInteraction.deferUpdate();
+        await confirmReplyMsg.delete();
+      }
+    } catch (err) {
+      // Timed out
+      await confirmReplyMsg.delete();
+      return;
+    }
+
     const { caseId, channelId, messageId } = customIDMatch.params;
 
     const dmChannel = await interaction.client.channels.fetch(channelId);
@@ -42,9 +116,9 @@ export default class DeleteModLogDMButtonHandler extends ButtonHandler {
         .setDescription("Hmm... couldn't find the channel.")
         .setColor(Color.Error);
 
-      await interaction.reply({
+      await confirmButtonInteraction.update({
         embeds: [embed.toJSON()],
-        ephemeral: true,
+        components: [],
       });
 
       return;
@@ -58,9 +132,9 @@ export default class DeleteModLogDMButtonHandler extends ButtonHandler {
         .setDescription("Hmm... the message is probably already deleted.")
         .setColor(Color.Error);
 
-      await interaction.reply({
+      await confirmButtonInteraction.update({
         embeds: [embed.toJSON()],
-        ephemeral: true,
+        components: [],
       });
 
       return;
@@ -81,9 +155,9 @@ export default class DeleteModLogDMButtonHandler extends ButtonHandler {
         .setDescription("Deleted the DM message")
         .setColor(Color.Success);
 
-      await interaction.reply({
-        embeds: [embed.toJSON()],
-        ephemeral: true,
+      await confirmButtonInteraction.update({
+        embeds: [embed],
+        components: [],
       });
 
       return;
@@ -95,7 +169,7 @@ export default class DeleteModLogDMButtonHandler extends ButtonHandler {
       true,
     );
 
-    await interaction.update({
+    await interaction.message.edit({
       embeds: interaction.message.embeds,
       components,
     });
