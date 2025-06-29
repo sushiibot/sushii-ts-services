6e36ae4486839e4f7ae3195c5f3b0e3d54e2f2a5
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2025-06-29 11:24:53 -0700

    chore: Add eslint-config-prettier

--- packages/sushii-worker/eslint.config.mjs
+++ packages/sushii-worker/eslint.config.mjs
@@ -2,11 +2,13 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  eslintConfigPrettier,
   // tseslint.configs.strictTypeChecked,
   // {
   //   languageOptions: {
