932b9d0e58d9c0c63922086711b97aeabcb5451e
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2025-07-23 18:19:01 -0700

    chore: Remove legacy notifications table and repo

--- packages/sushii-worker/src/db/Notification/NotificationBlock.table.ts
+++
@@ -1,8 +0,0 @@
-import { Insertable, Selectable, Updateable } from "kysely";
-import { AppPublicNotificationBlocks } from "../../infrastructure/database/dbTypes";
-
-export type NotificationBlockRow = Selectable<AppPublicNotificationBlocks>;
-export type InsertableNotificationBlockRow =
-  Insertable<AppPublicNotificationBlocks>;
-export type UpdateableNotificationBlockRow =
-  Updateable<AppPublicNotificationBlocks>;
