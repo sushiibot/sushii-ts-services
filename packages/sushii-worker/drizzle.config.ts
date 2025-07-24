59b93857026c90d57581d85c76df3d31ed6aa692
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2025-07-23 17:52:20 -0700

    fix: Update drizzle schema directory

--- packages/sushii-worker/drizzle.config.ts
+++ packages/sushii-worker/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/db/schema.ts",
+  schema: "./src/infrastructure/database/schema.ts",
   schemaFilter: ["app_public", "app_private", "app_hidden"],
   dialect: "postgresql",
   // Not necessary for postgres, only for databases that don't support multiple
